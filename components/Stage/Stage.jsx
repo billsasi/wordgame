a02ee08efe2b9b2be4a3df5690b0f0aa87b36376
Bill Sasi <billsasi@gmail.com>
2023-01-07 17:05:10 -0800

    getwords

--- components/Stage/Stage.jsx
+++ components/Stage/Stage.jsx
@@ -1,50 +1,66 @@
-import { useEffect, useRef, useReducer } from "react";
-import { playReducer } from "./play-reducer";
-import Worm from "./Worm";
+import { useEffect, useRef, useReducer } from 'react';
+import { playReducer } from './play-reducer';
+import Worm from './Worm';
+import getWords from '../../data/getWords';
+
+const initPlayers = (count) => {
+  const players = [];
+  for (let i = 0; i < count; i++) {
+    players.push({
+      currentWord: '',
+      id: i,
+      name: `player${i}`,
+      words: getWords(3),
+      position: {
+        x: 100 + i * 100,
+        y: 200,
+      },
+    });
+  }
+  return players;
+};
 
 const initialState = {
-    players: [{
-        currentWord: '',
-        id: 1,
-        name: 'player1',
-        words: ['hello', 'world', 'foo', 'bar'],
-        position: {
-            x: 100,
-            y: 200,
-        },
-    }]
-    };
+  players: initPlayers(4),
+};
 
 const Stage = (props) => {
-    const stageEl = useRef(null);
-    const [state, dispatch] = useReducer(playReducer, initialState);
-    const height = null, width = null;
-    const { players } = state;
-    const [player1] = players;
+  const stageEl = useRef(null);
+  const [state, dispatch] = useReducer(playReducer, initialState);
+  const height = null,
+    width = null;
+  const { players } = state;
+  const [player1] = players;
 
-    useEffect(() => {
-        setTimeout(() => {
-            console.log("stageEl", stageEl.current);
-        }, 1000);
-    }, []);
+  useEffect(() => {
+    console.log(getWords(10));
+    setTimeout(() => {
+      console.log('stageEl', stageEl.current);
+    }, 1000);
+  }, []);
 
-    return (
-        <div tabIndex="0" className="stage" ref={stageEl} onKeyUp={(event)=> dispatch({
-            type: 'KEY_UP',
-            payload: {
-                event
-            }
-        })}>
-            {
-                players.map((player) => {
-                    return (
-                        <Worm key={player.id} player={player} />
-                    )
-                })
-            }
-            <div style={{border: '1px solid red'}}>Current Word: {player1.currentWord}</div>
-        </div>
-    )
-}
+  return (
+    <div
+      tabIndex="0"
+      className="stage"
+      ref={stageEl}
+      onKeyUp={(event) =>
+        dispatch({
+          type: 'KEY_UP',
+          payload: {
+            event,
+          },
+        })
+      }
+    >
+      {players.map((player) => {
+        return <Worm key={player.id} player={player} />;
+      })}
+      <div style={{ border: '1px solid red' }}>
+        Current Word: {player1.currentWord}
+      </div>
+    </div>
+  );
+};
 
-export default Stage; 
+export default Stage;
