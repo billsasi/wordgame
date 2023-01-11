f1a0025a3c5f3399d32ffc3a736cd5df320bc52a
Bill Sasi <billsasi@gmail.com>
2023-01-10 16:38:20 -0800

    word entry

--- components/Stage/Stage.jsx
+++ components/Stage/Stage.jsx
@@ -7,6 +7,7 @@ const initPlayers = (count) => {
   const players = [];
   for (let i = 0; i < count; i++) {
     players.push({
+      wordEntryError: false,
       currentWord: '',
       id: i,
       name: `player${i}`,
@@ -33,37 +34,32 @@ const Stage = (props) => {
   const { players } = state;
   const [player1] = players;
 
-  const { currentWord } = player1;
+  const { currentWord, wordEntryError } = player1;
+
+  console.log('rerender');
 
   return (
-    <div
-      tabIndex="0"
-      className="stage"
-      ref={stageEl}
-      onKeyUp={(event) =>
-        dispatch({
-          type: 'KEY_DOWN',
-          payload: {
-            event,
-          },
-        })
-      }
-    >
+    <div tabIndex="0" className="stage" ref={stageEl}>
       <div className="players">
         {players.map((player) => {
           return <Worm key={player.id} player={player} />;
         })}
       </div>
 
-      <div style={{ border: '1px solid red' }}>
-        Current Word:
+      <div className="input-cntr">
         <input
+          className="input"
+          style={wordEntryError ? { borderColor: 'red' } : {}}
           ref={inputRef}
           value={currentWord}
           autoFocus
-          onChange={(e) => {
-            console.log('changed!');
-            dispatch({ type: 'INPUT_CHANGE', payload: e });
+          onKeyDown={(e) => {
+            if (e.code === 'Enter') {
+              dispatch({ type: 'KEY_UP', payload: e });
+            }
+          }}
+          onChange={({ target }) => {
+            dispatch({ type: 'INPUT_CHANGE', payload: target.value });
           }}
         ></input>
       </div>
--- components/Stage/play-reducer.js
+++ components/Stage/play-reducer.js
@@ -1,39 +1,48 @@
-export const playReducer = (state = {}, action, initData) => {
+const handleKeyUp = (state, action) => {
   const { players } = state;
   const [player1, ...restOfPlayers] = players;
-  switch (action.type) {
-    case 'KEY_DOWN':
-      if (action.payload.event.key === 'Enter') {
-        const entered = players[0].currentWord;
-        console.log(entered);
-        for (let i = 1; i < players.length; i++) {
-          console.log(players[i].words[0]);
-          if (players[i].words[0] === entered) {
-            players[0].currentWord = '';
-            players[i].words = players[i].words.slice(1);
-            players[0].words.push(entered);
-            return {
-              ...state,
-              players: [...players],
-            };
-          }
-        }
+  const entered = players[0].currentWord;
+  for (let i = 1; i < players.length; i++) {
+    for (let j = 0; j < players[i].words.length; j++) {
+      if (players[i].words[j] === entered) {
+        players[0].currentWord = '';
+        const victimIndex = players[i].words.indexOf(entered);
+        players[i].words.splice(victimIndex, 1);
+        players[0].words.push(entered);
         return {
           ...state,
-          players: [player1, ...restOfPlayers],
+          players: [...players],
         };
       }
+    }
+  }
+  return {
+    ...state,
+    players: [{ ...player1, wordEntryError: true }, ...restOfPlayers],
+  };
+};
+
+const handleInputChange = (state, action) => {
+  const newWord = action.payload;
+  const [player1, ...restOfPlayers] = state.players;
+  return {
+    ...state,
+    players: [
+      { ...player1, currentWord: newWord, wordEntryError: false },
+      ...restOfPlayers,
+    ],
+  };
+};
+
+export const playReducer = (state = {}, action, initData) => {
+  console.log(action, state);
+
+  switch (action.type) {
+    case 'KEY_UP':
+      return handleKeyUp(state, action);
 
     case 'INPUT_CHANGE':
-      const newWord = action.payload;
-      console.log(action.payload);
-      return {
-        ...state,
-        players: [
-          { ...player1, currentWord: player1.currentWord },
-          ...restOfPlayers,
-        ],
-      };
+      return handleInputChange(state, action);
     default:
       return state;
   }
